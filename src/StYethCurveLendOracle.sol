9220fc02fbd74713a45b3dcc73a038d7bc737885
0xValJohn <0xvaljohn@gmail.com>
2024-04-11 11:02:41 -0400

    feat: use different curve pools

--- src/StYethCurveLendOracle.sol
+++ src/StYethCurveLendOracle.sol
@@ -17,7 +17,8 @@ interface ICurveTricrypto {
 contract StYethCurveLendOracle {
     IStYETH public constant ST_YETH = IStYETH(0x583019fF0f430721aDa9cfb4fac8F06cA104d0B4);
     IStableSwap public constant YETH_ETH = IStableSwap(0x69ACcb968B19a53790f43e57558F5E443A91aF22);
-    ICurveTricrypto public constant TRI_CRV = ICurveTricrypto(0x4eBdF703948ddCEA3B11f675B4D1Fba9d2414A14);
+    IStableSwap public constant USDC_CRVUSD = IStableSwap(0x4DEcE678ceceb27446b35C672dC7d61F30bAD69E);
+    ICurveTricrypto public constant TRICRYPTO_USDC = ICurveTricrypto(0x7F86Bf177Dd4F3494b841a37e810A34dD56c829B);
 
     uint256 public constant RATE_MAX_SPEED = 166666666666666; // Max speed of rate change
 
@@ -48,7 +49,7 @@ contract StYethCurveLendOracle {
 
     function rawPrice() internal view returns (uint256) {
         return
-            (ST_YETH.totalAssets() * 1e18) / ST_YETH.totalSupply() * YETH_ETH.price_oracle() * TRI_CRV.price_oracle(0) / 1e36;
+            (ST_YETH.totalAssets() * 1e18) / ST_YETH.totalSupply() * TRICRYPTO_USDC.price_oracle(1) * USDC_CRVUSD.price_oracle() / 1e36;
     }
 
     function min(uint256 a, uint256 b) internal pure returns (uint256) {
